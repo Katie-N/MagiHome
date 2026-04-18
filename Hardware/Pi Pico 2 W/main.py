ebb4e116dbbbb46d769b50a252cdf784e1ce1ec7
LukeNschoolKU <lukenschoolku@gmail.com>
2026-04-18 07:59:19 -0500

    Update main.py

--- Hardware/Pi Pico 2 W/main.py
+++ Hardware/Pi Pico 2 W/main.py
@@ -8,7 +8,7 @@ import urequests
 # --- Configuration ---
 WIFI_SSID = "arctic"
 WIFI_PASS = "BreadofLife!"
-MQTT_BROKER = "192.168.12.243" # Example public broker
+MQTT_BROKER = "192.168.12.243" 
 MQTT_PORT = 1883
 MQTT_CLIENT_ID = "pico2w_client"
 MQTT_TOPIC = b"pico2w/test"
@@ -23,19 +23,32 @@ while not wlan.isconnected():
     time.sleep(1)
 print("Connected to Wi-Fi")
 
-# --- Connect to MQTT Broker ---
-# client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, port=MQTT_PORT, user=mqtt_username,  password=mqtt_password)
-# client.connect()
-# print(f"Connected to {MQTT_BROKER}")
-
-# --- Publish Message ---
-# try:
-#     message = b"Hello from Pico 2W"
-#     client.publish(MQTT_TOPIC, message)
-#     print(f"Published: {message} to {MQTT_TOPIC}")
-#      # Publish every 5 seconds
-# except KeyboardInterrupt:
-#     client.disconnect()
+ --- Connect to MQTT Broker ---
+client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, port=MQTT_PORT, user=mqtt_username,  password=mqtt_password)
+client.connect()
+ print(f"Connected to {MQTT_BROKER}")
+ --- Publish Message ---
+ config = {
+    "name": "Pico Sensors",
+    "unique_id": "pico__01",
+    "state_topic": "home/pico/sensor",
+    "unit_of_measurement": "°C",
+    "device_class": "temperature",
+    "value_template": "{{ value_json.temperature }}",
+    "device": {
+        "identifiers": [CLIENT_ID],
+        "name": "Raspberry Pi Pico 2W",
+        "model": "Pico 2W",
+        "manufacturer": "Raspberry Pi"
+    }
+}
+try:
+    message = b"Hello from Pico 2W"
+    client.publish(MQTT_TOPIC, message)
+     print(f"Published: {message} to {MQTT_TOPIC}")
+     # Publish every 5 seconds
+except KeyboardInterrupt:
+        client.disconnect()
 
 # Initialize UART
 uart = machine.UART(0, baudrate=9600, tx=machine.Pin(0), rx=machine.Pin(1))
@@ -74,12 +87,14 @@ while True:
                 payload = json.load(f)
 
                 # 2. Push to the target IP address
-                url = 'http://10.154.115.99:3000'
+                url = 'http://192.168.12.99:3000'
+                # 10.154.115.99:3000
                 try:
                     response = urequests.post(url, json=payload)
                     print("Status:", response.status_code)
                     print("Response:", response.text)
                     response.close() # Always close to free memory
                 except Exception as e:
+                    
                     print("Error:", e)
         
